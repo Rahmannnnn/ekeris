b06c45f6df7b45125b67a71aa22f2ff84ad2019c
rahmannnnn <arifingatrahman@gmail.com>
2024-05-14 02:15:27 +0700

    change cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const HistoryModel = require("./models/Histories");
 const app = express();
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["https://ekeris.vercel.app"],
     methods: ["POST", "GET", "PUT", "DELETE"],
     credentials: true,
   })
